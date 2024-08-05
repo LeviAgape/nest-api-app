a7449d8c73fd5150239cba4aa94e846c7e3cab18
Levi <agapelevi99@gmail.com>
2024-08-05 06:32:11 -0300

    fix: seed prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,40 +3,30 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function run() {
-  const post1 = await prisma.post.create({
-    data: {
-      title: "Primeiro Post",
-      description: "Descrição do primeiro post",
-    },
+  await prisma.post.deleteMany();
+  await prisma.member.deleteMany();
+  
+  await prisma.post.createMany({
+    data: [
+      { title: 'Post 1', description: 'Descrição do Post 1' },
+      { title: 'Post 2', description: 'Descrição do Post 2' },
+    ],
   });
 
-  const post2 = await prisma.post.create({
-    data: {
-      title: "Segundo Post",
-      description: "Descrição do segundo post",
-    },
+  await prisma.member.createMany({
+    data: [
+      { user: 'User 1', age: 25 },
+      { user: 'User 2', age: 30 },
+    ],
   });
 
-  const post3 = await prisma.member.create({
-    data: {
-      user: "Primeiro User",
-      age: 17,
-    },
-  });
-
-  const post4 = await prisma.member.create({
-    data: {
-      user: "Segundo User",
-      age: 20,
-    },
-  });
 }
 
 run()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
+  .catch((e) => {
     console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
     await prisma.$disconnect();
   });
